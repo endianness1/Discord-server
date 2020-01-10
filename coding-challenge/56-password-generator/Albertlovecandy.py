34c7889b35d4ee1829afe7ddcf4435fc772aa07f
AlbertCoolGuy <39652844+AlbertCoolGuy@users.noreply.github.com>
2020-01-10 15:45:38 +0100

    Create Albertlovecandy.py

---
+++ coding-challenge/56-password-generator/Albertlovecandy.py
@@ -0,0 +1,24 @@
+import random
+import string
+
+def split(word):
+    return [char for char in word]
+charset = split(string.ascii_letters + string.digits)
+
+def random_character():
+    random_char = charset[random.randint(0,len(charset))]
+    return random_char
+password = ""
+i = 0
+x = 0
+#while i < 10:
+   #password = random_char + password
+   #i += 1
+#print(password)
+while i < 10:
+    password = password + random_character()
+    i += 1
+    x = 1
+while len(password) == 10 and x == 1:
+    print(password)
+    x = 0
